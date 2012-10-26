c5298b1dfce255163efb06afa095e780dcabe4d7
Jonas Baumann <jone@jone.ch>
2012-10-26 15:09:29 +0200

    Rename readme to .rst.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ version = open('simplelayout/ui/base/version.txt').read().strip()
 setup(name='simplelayout.ui.base',
       version=version,
       description="Simplelayout ui base package - for plone",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
